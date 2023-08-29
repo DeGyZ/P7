ef8521532268d696cfca2d295659c53bde0975fa
DeGyZ <119336329+DeGyZ@users.noreply.github.com>
2023-08-29 18:34:34 +0200

    ajout frontend

---
+++ backend/routes/book.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const auth = require('../middleware/auth');
+const router = express.Router();
+const multer = require('../middleware/multer-config');
+
+
+const bookCtrl = require('../controllers/book');
+
+router.post('/', auth, multer, bookCtrl.createBook);
+router.put('/:id', auth, multer, bookCtrl.modifyBook);
+router.delete(':id', auth, bookCtrl.deleteBook);
+router.get(':id', auth, bookCtrl.getOneBook);
+router.get('/', auth, bookCtrl.getAllBook);
+
+module.exports = router;
